348124cd6e40b49048dee0aa0a2b08b5fe3c8d64
PabloGLeiteDeSouza <pablo.guilherme.leite.de.souza@gmail.com>
2025-02-20 19:23:38 -0300

    commit to verify bug in FormCadastrarClientes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Ecommerce from "@/components/Ecommerce";
+import FormCadastrarClientes from "@/components/Forms/clientes/cadastrar";
 import { Box } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Ecommerce>
       <Box>
-        
+        <FormCadastrarClientes />
       </Box>
     </Ecommerce>
   );
